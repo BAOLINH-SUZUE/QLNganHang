d16647be3f71e7cd2fa97d304e255dc77cff3598
DOANLINHFFFF <119753910+DOANLINHFFFF@users.noreply.github.com>
2023-04-06 10:03:09 +0700

    them tao tinh dung

---
+++ QLNganHang/fTinDung.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNganHang
+{
+    public partial class fTinDung : Form
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public fTinDung()
+        {
+            InitializeComponent();
+        }
+
+        private void fTinDung_Load(object sender, EventArgs e)
+        {
+            db = new DataClasses1DataContext();
+            gvKhachHang.DataSource = db.KhachHangs;
+        }
+
+        private void txtTimKiem_TextChanged(object sender, EventArgs e)
+        {
+            var lst = (from s in db.KhachHangs where s.Cccd.Contains(txtTimKiem.Text) select s).ToList();
+            gvKhachHang.DataSource = lst;
+
+        }
+
+        private void btnTaoTinDung_Click(object sender, EventArgs e)
+        {
+            
+            fTaoTinDung f = new fTaoTinDung();
+            f.ShowDialog();
+
+            
+        }
+    }
+}
