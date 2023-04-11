39745281b6bfcef44ba7349c053000f6738c8569
DOANLINHFFFF <119753910+DOANLINHFFFF@users.noreply.github.com>
2023-04-11 16:31:00 +0700

    them Tk, TK, AC

--- QLNganHang/Account.cs
+++ QLNganHang/Account.cs
@@ -57,5 +57,4 @@ namespace QLNganHang
             set { userName = value; }
         }
     }
-
 }
---
+++ QLNganHang/tinDungcs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNganHang
+{
+    internal class tinDungcs
+    {
+        public string MaTD;
+        public string TenKH;
+        public SqlMoney ThuNhap;
+        public string Cccd;
+        public string LoaiThe;
+        public string HangMuc;
+        public DateTime NgayTra;
+        public float laiXuatTD;
+        public SqlMoney NoTD;
+        public SqlMoney NoXau;
+        public string SoLan;
+
+        public tinDungcs(string matd, string tenkh, SqlMoney thunhap, string cccd, string loaithe, string hangmuc, DateTime ngaytra, float laixuattd, SqlMoney notd, SqlMoney noxau, string solan)
+        {
+            MaTD = matd;
+            TenKH = tenkh;
+            ThuNhap = thunhap;
+            Cccd = cccd;
+            LoaiThe = loaithe;
+            HangMuc = hangmuc;
+            NgayTra = ngaytra;
+            this.laiXuatTD = laixuattd;
+            NoTD = notd;
+            NoXau = noxau;
+            SoLan = solan;
+        }
+    }
+}
