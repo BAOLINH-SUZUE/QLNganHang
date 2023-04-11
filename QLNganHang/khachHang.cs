3b16a148380c1c7370f4a4cdc6d3f4bc27e5d60e
DOANLINHFFFF <119753910+DOANLINHFFFF@users.noreply.github.com>
2023-04-11 15:50:22 +0700

    them class KhachHang

---
+++ QLNganHang/khachHang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNganHang
+{
+    internal class khachHang
+    {
+        public string MaKH;
+        public string TenKH;
+        public DateTime NgaySinh;
+        public string SDT;
+        public string Email;
+        public string Cccd;
+        public string Diachi;
+        public SqlMoney ThuNhap;
+
+        public khachHang(string makh, string tenkh, DateTime ngaysinh, string sdt, string email, string cccd, string diachi, SqlMoney thuNhap)
+        {
+            MaKH = makh;
+            TenKH = tenkh;
+            NgaySinh = ngaysinh;
+            SDT = sdt;
+            Email = email;
+            Cccd = cccd;
+            Diachi = diachi;
+            ThuNhap = thuNhap;
+        }
+    }
+}
